f06dd71bff8e3eab3899976ec34b057f998a195f
Dakota Smith <dsmith@appareo.com>
2017-03-09 17:51:36 -0700

    Setup HTTP client to handle auth and cert validation

--- tsukurogami.go
+++ tsukurogami.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"crypto/tls"
 	"flag"
+	"net/http"
+	"encoding/base64"
 )
 
 var xcodeURL = flag.String("xcode-url", "", "The url of your xcode server")
@@ -10,7 +14,22 @@ var xcodeCredentials = flag.String("xcode-credentials", "", "The credentials for
 var bitbucketCredentials = flag.String("bitbucket-credentials", "", "The credentials for your bitbucket server. username:password")
 var port = flag.Int("port", 4444, "The port to listen on")
 var configFilePath = flag.String("config", "", "(Optional) Path to configuration file")
+var skipVerify = flag.Bool("skip-verify", true, "Skip certification verification on the xcode server")
+
+var client *http.Client
+
+type transport struct {
+	http.Transport
+	creds string
+}
+
+func (t transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	auth := fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(t.creds)))
+	req.Header.Add("Authorization", auth)
+	return t.Transport.RoundTrip(req)
+}
 
 func main() {
-
+	flag.Parse()
+	client = &http.Client{Transport: transport{creds: *xcodeCredentials, Transport: http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: *skipVerify}}}}
 }
