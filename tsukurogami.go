4621e727658377d9d5aecd39ba6fb31ee670df9e
Dakota Smith <dsmith@appareo.com>
2017-03-10 11:12:47 -0700

    Added code to receive pull request information and send to stubs to create/delete bots

--- tsukurogami.go
+++ tsukurogami.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"log"
 	"fmt"
 	"crypto/tls"
 	"flag"
 	"net/http"
+	"strings"
 	"encoding/base64"
 )
 
-var xcodeURL = flag.String("xcode-url", "", "The url of your xcode server")
-var bitbucketURL = flag.String("bitbucket-url", "", "The url of your bitbucket server")
-var xcodeCredentials = flag.String("xcode-credentials", "", "The credentials for your xcode server. username:password")
-var bitbucketCredentials = flag.String("bitbucket-credentials", "", "The credentials for your bitbucket server. username:password")
-var port = flag.Int("port", 4444, "The port to listen on")
-var configFilePath = flag.String("config", "", "(Optional) Path to configuration file")
-var skipVerify = flag.Bool("skip-verify", true, "Skip certification verification on the xcode server")
+// flags
+var (
+	xcodeURL = flag.String("xcode-url", "", "The url of your xcode server")
+	bitbucketURL = flag.String("bitbucket-url", "", "The url of your bitbucket server")
+	xcodeCredentials = flag.String("xcode-credentials", "", "The credentials for your xcode server. username:password")
+	bitbucketCredentials = flag.String("bitbucket-credentials", "", "The credentials for your bitbucket server. username:password")
+	port = flag.Int("port", 4444, "The port to listen on")
+	// configFilePath = flag.String("config", "", "(Optional) Path to configuration file")
+	skipVerify = flag.Bool("skip-verify", true, "Skip certification verification on the xcode server")
+	template = flag.String("template", "$REPO_NAME.continuous", "The bot from which settings should be copied")
+)
 
 var client *http.Client
 
@@ -29,7 +35,57 @@ func (t transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	return t.Transport.RoundTrip(req)
 }
 
+func handlePullRequestUpdated(w http.ResponseWriter, r *http.Request) {
+
+	repo, ok := r.URL.Query()["repo"]
+	if !ok || len(repo) < 1 {
+		w.WriteHeader(400)
+		w.Write([]byte(`missing "repo" parameter`))
+		return
+	}
+
+	branch, ok := r.URL.Query()["branch"]
+	if !ok || len(branch) < 1 {
+		w.WriteHeader(400)
+		w.Write([]byte(`missing "branch" parameter`))
+		return
+	}
+
+	status, ok := r.URL.Query()["status"]
+	if !ok || len(status) < 1 {
+		w.WriteHeader(400)
+		w.Write([]byte(`missing "status" parameter`))
+		return
+	}
+
+	switch strings.ToLower(status[0]) {
+	case "opened", "reopened":
+		err := createBot(repo[0], branch[0])
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "error creating bot: %s\n", err)
+	case "closed", "declined":
+		err := deleteBot(repo[0], branch[0])
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "error deleting bot: %s\n", err)
+	default:
+		// nop
+	}
+
+	w.WriteHeader(201)
+}
+
+func createBot(repo, branch string) error {
+	return nil
+}
+
+func deleteBot(repo, branch string) error {
+	return nil
+}
+
 func main() {
 	flag.Parse()
 	client = &http.Client{Transport: transport{creds: *xcodeCredentials, Transport: http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: *skipVerify}}}}
+
+	http.HandleFunc("/pullRequestUpdated", handlePullRequestUpdated)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
